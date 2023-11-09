d3d99e9aa1fb507d1cb343c54bec8b251389b8d9
LRYMND <louis.raymond@outlook.de>
2023-11-09 15:17:14 +0100

    Python-Vite-React-Carplay Boilerplate

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory, render_template
 from flask_socketio import SocketIO
 import os
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with your own secret key
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '..', 'dist'), static_folder=os.path.join(os.path.dirname(__file__), '..', 'dist', 'assets'), static_url_path='/assets')
+app.config['SECRET_KEY'] = 'your_secret_key'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# Get the path to the frontend/dist folder relative to the backend/app.py file.
-frontend_dist_path = os.path.join(os.path.dirname(__file__), '..', 'dist')
-
-# Define a route to serve your React app.
+# Route to serve the index.html file
 @app.route('/')
-def index():
-    # Construct the path to the index.html file in the React app's 'dist' folder.
-    index_path = os.path.join(frontend_dist_path, 'index.html')
+def serve_index():
+    return render_template('index.html')
 
-    # Render the index.html file.
-    return render_template(index_path)
+# Route to serve static files (js, css, etc.) from the 'dist/assets' folder
+@app.route('/assets/<path:filename>')
+def serve_assets(filename):
+    return send_from_directory(os.path.join(os.path.dirname(__file__), '..', 'dist', 'assets'), filename)
 
 # Define additional routes and API endpoints as needed.
 @app.route('/api/data', methods=['GET'])
@@ -25,8 +23,8 @@ def get_data():
     return data, 200
 
 @socketio.on('connect')
-def test_connect():
-    print("client connected")
+def handle_connect():
+    print("Client connected")
 
 @socketio.on('FRONTEND_MESSAGE', namespace='/')
 def handle_frontend_message(message):
@@ -34,7 +32,5 @@ def handle_frontend_message(message):
     # You can broadcast the message to all connected clients if needed.
     socketio.emit('BACKEND_MESSAGE', "HELLO FROM BACKEND")
 
-
-
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=4001)  # Adjust the host and port as needed.
+    socketio.run(app, host='0.0.0.0', port=4001)
--- vite.config.ts
+++ vite.config.ts
@@ -26,8 +26,8 @@ export default defineConfig({
       },
       plugins: [
         NodeGlobalsPolyfillPlugin({
-            buffer: true
-          }),
+          buffer: true
+        }),
         NodeModulesPolyfillPlugin()
       ]
     }

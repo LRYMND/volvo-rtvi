97ea6afdb0a88e6184c8ff8bdf3d859a563a08e7
LRYMND <louis.raymond@outlook.de>
2023-11-12 01:46:28 +0100

    Removed mui

--- src/content/sidebars/NavBar.tsx
+++ src/content/sidebars/NavBar.tsx
@@ -17,26 +17,25 @@ const NavBar = ({ applicationSettings, view, setView }) => {
 
   return (
     <div className={`navbar ${applicationSettings.app.colorTheme.value}`} style={{ backgroundImage: `url(${NavBarBackground})` }}>
-
-          <IconButton onClick={() => changeView('Dashboard')} style={{ fill: (view === 'Dashboard') ? 'var(--fillActive)' : 'var(--fillInactive)' }}>
-            <svg className="navbar__icon">
-              <use xlinkHref="./svg/gauge.svg#gauge"></use>
-            </svg>
-          </IconButton>
+					<button className="custom-button" onClick={() => changeView('Dashboard')} style={{ fill: (view === 'Dashboard') ? 'var(--fillActive)' : 'var(--fillInactive)' }}>
+  					<svg xmlns="http://www.w3.org/2000/svg" className="navbar__icon">
+    					<use xlinkHref="./svg/gauge.svg#gauge"></use>
+  					</svg>
+					</button>
 
           {applicationSettings.interface.activateMMI.value ?
-            <IconButton onClick={() => changeView('Carplay')} style={{ fill: (view === 'Carplay') ? 'var(--fillActive)' : 'var(--fillInactive)' }}>
-              <svg className="navbar__icon">
-                <use xlinkHref="./svg/carplay.svg#carplay"></use>
-              </svg>
-            </IconButton>
-          : <></>}
-
-          <IconButton onClick={() => changeView('Settings')} style={{ fill: (view === 'Settings') ? 'var(--fillActive)' : 'var(--fillInactive)' }}>
-            <svg className="navbar__icon">
-              <use xlinkHref="./svg/settings.svg#settings"></use>
-            </svg>
-          </IconButton>
+						<button className="custom-button" onClick={() => changeView('Carplay')} style={{ fill: (view === 'Carplay') ? 'var(--fillActive)' : 'var(--fillInactive)' }}>
+  						<svg xmlns="http://www.w3.org/2000/svg" className="navbar__icon">
+    						<use xlinkHref="./svg/carplay.svg#carplay"></use>
+  						</svg>
+						</button>
+						: <></>}
+					
+					<button className="custom-button" onClick={() => changeView('Settings')} style={{ fill: (view === 'Settings') ? 'var(--fillActive)' : 'var(--fillInactive)' }}>
+  					<svg xmlns="http://www.w3.org/2000/svg" className="navbar__icon">
+    					<use xlinkHref="./svg/settings.svg#settings"></use>
+  					</svg>
+					</button>
 
 
       {/*
